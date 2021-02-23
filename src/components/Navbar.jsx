8e7ea8def3ceac5f9d24336e5b32da5ae77eeeda
Matthew Greenspun <matthewgreenspun@gmail.com>
2021-02-22 22:03:37 -0500

    changed styles and made navbar show which page user is on

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,15 @@
-import {Link} from "react-router-dom"
+import {Link} from "react-router-dom";
+import {useState} from "react";
 
 export default function Navbar(props) {
+    const [isOnMyPolls, setIsOnMyPolls] =  useState(false);
+    const underline = {borderBottom: "5px solid blue"};
     const {id} = props;
     return (
         <nav className = "navbar">
             <div className="logo nav-item">Polling App</div>
-            <Link to = {"/users" + id} className = "nav-item nav-tab">My Polls</Link>
-            <Link to = "/users" className = "nav-item nav-tab">All Polls</Link>
+            <Link to = {"/users" + id} className = "nav-item nav-tab" style={isOnMyPolls? underline : {}} onClick = {() => setIsOnMyPolls(true)} >My Polls</Link>
+            <Link to = "/users" className = "nav-item nav-tab" style = {!isOnMyPolls? underline : {}}  onClick = {() => setIsOnMyPolls(false)} >All Polls</Link>
         </nav>
     );
 }
