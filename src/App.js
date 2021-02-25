646f94c56c64870586c23812af3bc9bf061838a0
Matthew Greenspun <matthewgreenspun@gmail.com>
2021-02-24 22:33:51 -0500

    no longer using react-router-dom and added axios to dependencies

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import UserApp from "./components/UserApp";
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import axios from "axios";
+import Navbar from "./components/Navbar";
+import Poll from "./components/Poll";
+import Sidebar from "./components/Sidebar";
+import {useState, useEffect} from "react";
 
 export default function App() {
-  return (
-    <div className = "app">
-      <Router>
-        <Route path = "/" component = {UserApp}/>
-      </Router>
-    </div>
-  );
+    return (
+        <section className = "user-app">
+            <Navbar />
+            <div className = "user-profile">
+                <Sidebar sidebarObj = {{username: "Test User", totalVotes: 827}} />
+                <section className = "polls">
+                    <Poll 
+                        pollObj = {{
+                            pollTitle: "Favorite Letter", 
+                            pollItems: [
+                                {itemName: 'a', votes: 1, percentage: 25},
+                                {itemName: 'a', votes: 1, percentage: 25},
+                                {itemName: 'a', votes: 1, percentage: 25},
+                                {itemName: 'a', votes: 1, percentage: 25},
+                            ]
+                        }} 
+                    /> 
+                </section>
+            </div>
+        </section>
+    );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,13 @@
-import {Link} from "react-router-dom";
 import {useState} from "react";
 
 export default function Navbar(props) {
     const [isOnMyPolls, setIsOnMyPolls] =  useState(false);
     const underline = {borderBottom: "5px solid blue"};
-    const {id} = props;
     return (
         <nav className = "navbar">
             <div className="logo nav-item">Polling App</div>
-            <Link to = {"/users" + id} className = "nav-item nav-tab" style={isOnMyPolls? underline : {}} onClick = {() => setIsOnMyPolls(true)} >My Polls</Link>
-            <Link to = "/users" className = "nav-item nav-tab" style = {!isOnMyPolls? underline : {}}  onClick = {() => setIsOnMyPolls(false)} >All Polls</Link>
+            <div  className = "nav-item nav-tab" style={isOnMyPolls? underline : {}} onClick = {() => setIsOnMyPolls(true)} >My Polls</div>
+            <div className = "nav-item nav-tab" style = {!isOnMyPolls? underline : {}}  onClick = {() => setIsOnMyPolls(false)} >All Polls</div>
         </nav>
     );
 }
