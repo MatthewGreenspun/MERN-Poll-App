75d018f33eef4a4552ff7989ab934b206fdb22b9
Matthew Greenspun <matthewgreenspun@gmail.com>
2021-03-11 21:35:11 -0500

    requesting nonexistant user id results in status 400

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -11,13 +11,13 @@ router.route("/").get((req, res) => {
 router.route("/:id").get((req, res) => {
     User.findById(req.params.id)
         .then((user) => res.json(user))
-        .catch((err) => res.json("Error: " + err));
+        .catch((err) => res.status(400).json("Error: " + err));
 });
 
 router.route("/:id").delete((req, res) => {
     User.findByIdAndDelete(req.params.id)
         .then(() => res.json("User Deleted"))
-        .catch((err) => res.json("Error: " + err));
+        .catch((err) => res.status(400).json("Error: " + err));
 });
 
 router.route("/add").post((req, res) => {
