ede0f355d1a5d7c0e6483c3c09484fca1e380254
Matthew Greenspun <matthewgreenspun@gmail.com>
2021-03-17 21:41:56 -0400

    users can view other user's polls in the all polls tab and data is refetched when user changes tabs

--- src/components/AccountPage.jsx
+++ src/components/AccountPage.jsx
@@ -3,88 +3,162 @@ import Poll from "./Poll";
 import Sidebar from "./Sidebar";
 import NewPollForm from "./NewPollForm";
 import PollListSorter from "./PollListSorter";
-import {useState, useEffect} from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
-export default function AccountPage({userId, userData}) {
-    const [user, setUser] = useState(null);
-    const [totalVotes, setTotalVotes] = useState(0);
-    const [isOnMyPolls, setIsOnMyPolls] = useState(true); 
-    const [polls, setPolls] = useState([]);
-    const [pollsOrder, setPollsOrder] = useState("Old - New");
-    const [isAddingPoll, setIsAddingPoll] = useState(false);
+export default function AccountPage({ userId, userData }) {
+  const [user, setUser] = useState(null);
+  const [totalVotes, setTotalVotes] = useState(0);
+  const [isOnMyPolls, setIsOnMyPolls] = useState(true);
+  const [polls, setPolls] = useState([]);
+  const [pollsOrder, setPollsOrder] = useState("Old - New");
+  const [isAddingPoll, setIsAddingPoll] = useState(false);
 
-    useEffect(() => {
-        async function fetchUserData() {
-            const json = await userData;
-            if(json.polls === undefined) return;
+  function formatUserData(userDataJson) {
+    let tmpTotalVotes = 0;
+    let tmpPolls = [];
+    userDataJson.polls.forEach((poll) => {
+      let newPollObj = {
+        pollTitle: poll.pollTitle,
+        id: poll.id,
+        pollItems: [],
+        totalVotes: 0,
+        polsterId: userDataJson._id,
+      };
+      Object.keys(poll.pollOptions).forEach((option) => {
+        tmpTotalVotes += poll.pollOptions[option];
+        newPollObj.totalVotes += poll.pollOptions[option];
+        newPollObj.pollItems.push({
+          itemName: option,
+          votes: poll.pollOptions[option],
+        });
+      });
+      tmpPolls.push(newPollObj);
+    });
 
-            let tmpTotalVotes = 0;
-            let tmpPolls = [];
-            json.polls.forEach((poll) => {
-                let newPollObj = {pollTitle: poll.pollTitle, id: poll.id, pollItems: [], totalVotes: 0};
-                Object.keys(poll.pollOptions).forEach((option) => {
-                    tmpTotalVotes += poll.pollOptions[option];
-                    newPollObj.totalVotes += poll.pollOptions[option];
-                    newPollObj.pollItems.push({itemName: option, votes: poll.pollOptions[option]});
-                });
-                tmpPolls.push(newPollObj);
-            });
+    return [tmpTotalVotes, tmpPolls];
+  }
 
-            setPolls(tmpPolls);
-            setTotalVotes(tmpTotalVotes);
-            setUser(json);
-        }
-        fetchUserData();
-    }, [userData])
-
-    function handlePollOrderChange(sortingOrder) {
-        if(sortingOrder === "Old - New") setPolls(polls.sort((poll1, poll2) => poll1.id - poll2.id));
-        if(sortingOrder === "New - Old") setPolls(polls.sort((poll1, poll2) => poll2.id - poll1.id));
-        if(sortingOrder === "Votes Ascending") setPolls(polls.sort((poll1, poll2) => poll1.totalVotes - poll2.totalVotes));
-        if(sortingOrder === "Votes Descending") setPolls(polls.sort((poll1, poll2) => poll2.totalVotes - poll1.totalVotes));
-        setPollsOrder(sortingOrder)
-    }
-
-    return (
-        <section className = "user-app">
-            <Navbar 
-                isOnMyPolls = {isOnMyPolls}
-                onChangeTab = {(tab) => setIsOnMyPolls(tab)}
-            />
-            <div className = "user-profile">
-                {isOnMyPolls && <Sidebar 
-                    sidebarObj = {{
-                        username: user === null? "Loading ..." : user.username, 
-                        userId,
-                        totalVotes: user === null? 0 : totalVotes,
-                        numberOfPolls: polls.length,
-                        dateJoined: user === null? null : user.createdAt.slice(0,10),
-                    }} 
-                />}
-                <section className = "polls">
-                    <PollListSorter 
-                        numberOfPolls={polls.length} 
-                        value={pollsOrder}
-                        onSortingChange={(sortingOrder) => handlePollOrderChange(sortingOrder)}
-                    />
-
-                    {polls.map((poll, i) => (
-                        <Poll 
-                            key = {i}
-                            userId = {userId}
-                            pollObj = {poll}
-                        />
-                    ))}
-
-                    {!isAddingPoll && isOnMyPolls && <div onClick = {() => setIsAddingPoll(true)} className = "poll" style={{marginBottom: "1rem"}}>
-                        <h2>+ Add Poll</h2>
-                    </div>}
-                    {isAddingPoll && isOnMyPolls && <NewPollForm 
-                        onSubmit = {() => setIsAddingPoll(false)}
-                        userId = {userId}
-                    />}
-                </section>
-            </div>
-        </section>
+  async function fetchUserData() {
+    const userDataJson = await axios.get(
+      `http://localhost:5000/users/${userId}`
     );
+    const [newTotalVotes, newPolls] = formatUserData(userDataJson.data);
+    setPolls(newPolls);
+    setTotalVotes(newTotalVotes);
+    setUser(userDataJson.data);
+    setPollsOrder("Old - New");
+  }
+
+  async function fetchAllPolls() {
+    const allUsersJson = await axios.get("http://localhost:5000/users");
+    let allUsersPolls = [];
+    allUsersJson.data.forEach((user) => {
+      user.polls.forEach((poll) => {
+        let newPollObj = {
+          pollTitle: poll.pollTitle,
+          id: poll.id,
+          pollItems: [],
+          totalVotes: 0,
+          polsterId: user._id,
+        };
+        Object.keys(poll.pollOptions).forEach((option) => {
+          newPollObj.totalVotes += poll.pollOptions[option];
+          newPollObj.pollItems.push({
+            itemName: option,
+            votes: poll.pollOptions[option],
+          });
+        });
+        allUsersPolls.push(newPollObj);
+      });
+    });
+    setPolls(allUsersPolls);
+    setPollsOrder("Old - New");
+  }
+
+  useEffect(() => {
+    const json = userData;
+    if (json.polls === undefined) return;
+    const [newTotalVotes, newPolls] = formatUserData(json);
+
+    setPolls(newPolls);
+    setTotalVotes(newTotalVotes);
+    setUser(json);
+  }, [userData]);
+
+  useEffect(() => {
+    if (isOnMyPolls) {
+      fetchUserData();
+    } else {
+      fetchAllPolls();
+    }
+    //eslint-disable-next-line
+  }, [isOnMyPolls]);
+
+  function handlePollOrderChange(sortingOrder) {
+    if (sortingOrder === "Old - New")
+      setPolls(polls.sort((poll1, poll2) => poll1.id - poll2.id));
+    if (sortingOrder === "New - Old")
+      setPolls(polls.sort((poll1, poll2) => poll2.id - poll1.id));
+    if (sortingOrder === "Votes Ascending")
+      setPolls(
+        polls.sort((poll1, poll2) => poll1.totalVotes - poll2.totalVotes)
+      );
+    if (sortingOrder === "Votes Descending")
+      setPolls(
+        polls.sort((poll1, poll2) => poll2.totalVotes - poll1.totalVotes)
+      );
+    setPollsOrder(sortingOrder);
+  }
+
+  return (
+    <section className="user-app">
+      <Navbar
+        isOnMyPolls={isOnMyPolls}
+        onChangeTab={(tab) => setIsOnMyPolls(tab)}
+      />
+      <div className="user-profile">
+        {isOnMyPolls && (
+          <Sidebar
+            sidebarObj={{
+              username: user === null ? "Loading ..." : user.username,
+              userId,
+              totalVotes: user === null ? 0 : totalVotes,
+              numberOfPolls: polls.length,
+              dateJoined: user === null ? null : user.createdAt.slice(0, 10),
+            }}
+          />
+        )}
+        <section className="polls">
+          <PollListSorter
+            numberOfPolls={polls.length}
+            value={pollsOrder}
+            onSortingChange={(sortingOrder) =>
+              handlePollOrderChange(sortingOrder)
+            }
+          />
+
+          {polls.map((poll, i) => (
+            <Poll key={i} userId={poll.polsterId} pollObj={poll} />
+          ))}
+
+          {!isAddingPoll && isOnMyPolls && (
+            <div
+              onClick={() => setIsAddingPoll(true)}
+              className="poll"
+              style={{ marginBottom: "1rem" }}
+            >
+              <h2>+ Add Poll</h2>
+            </div>
+          )}
+          {isAddingPoll && isOnMyPolls && (
+            <NewPollForm
+              onSubmit={() => setIsAddingPoll(false)}
+              userId={userId}
+            />
+          )}
+        </section>
+      </div>
+    </section>
+  );
 }
--- src/components/NewPollForm.jsx
+++ src/components/NewPollForm.jsx
@@ -1,78 +1,109 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 export default function NewPollForm(props) {
-    const {userId} = props;
-    const [pollTitle, setPollTitle] = useState("My Poll");
-    const [pollOptions, setPollOptions] = useState([]);
+  const { userId } = props;
+  const [pollTitle, setPollTitle] = useState("My Poll");
+  const [pollOptions, setPollOptions] = useState([]);
 
-    function deleteOption(optionName){
-        setPollOptions(pollOptions.filter((option) => option !== optionName));
-    }
+  function deleteOption(optionName) {
+    setPollOptions(pollOptions.filter((option) => option !== optionName));
+  }
 
-    function addOption(optionName) {
-        let newOptions = pollOptions;
-        newOptions.push(optionName);
-        setPollOptions([...newOptions]);
-    }
+  function addOption(optionName) {
+    let newOptions = pollOptions;
+    newOptions.push(optionName);
+    setPollOptions([...newOptions]);
+  }
 
-    function handleSubmit(e) {
-        e.preventDefault();
-        let pollJSONForBackend = {pollTitle, pollOptions: {}};
-        pollOptions.forEach((option) => pollJSONForBackend.pollOptions[option] = 0);
-        console.log(pollJSONForBackend);
-        axios.post(`http://localhost:5000/users/newpoll/${userId}`, pollJSONForBackend);
-        props.onSubmit();
-    }
+  function handleSubmit(e) {
+    e.preventDefault();
+    let pollJSONForBackend = { pollTitle, pollOptions: {} };
+    pollOptions.forEach(
+      (option) => (pollJSONForBackend.pollOptions[option] = 0)
+    );
+    console.log(pollJSONForBackend);
+    axios.post(
+      `http://localhost:5000/users/newpoll/${userId}`,
+      pollJSONForBackend
+    );
+    props.onSubmit();
+  }
 
-    return (
-        <div className = "poll" id="poll-creator">
-            <div className="poll-form-option" onClick={() => props.onSubmit()} style={{float:"right", backgroundColor:"#f00"}}>X</div>
-            <div className="form-title-container">
-                <h2>Title: </h2>
-                <input type="text" className="poll-form-title form-input" onChange={(e) => setPollTitle(e.target.value)} value={pollTitle}/>
-            </div>
-            <h2>Options: </h2>
-            <div className="poll-form-options">
-                {pollOptions.map((option, i) => <PollOption 
-                    className = "poll-form-option"
-                    key = {i}
-                    name = {option} 
-                    onDelete = {(optionName) => deleteOption(optionName)}
-                />)}
-                <PollOptionCreator onAddOption={(optionName) => addOption(optionName)}/> 
-            </div>
-            <form>    
-                <button type = "submit" className="create-poll-btn" onClick = {(e) => handleSubmit(e)}>Create Poll</button>
-            </form>
-        </div>
-    )
+  return (
+    <div className="poll" id="poll-creator">
+      <div
+        className="poll-form-option"
+        onClick={() => props.onSubmit()}
+        style={{ float: "right", backgroundColor: "#f00" }}
+      >
+        X
+      </div>
+      <div className="form-title-container">
+        <h2>Title: </h2>
+        <input
+          type="text"
+          className="poll-form-title form-input"
+          onChange={(e) => setPollTitle(e.target.value)}
+          value={pollTitle}
+        />
+      </div>
+      <h2>Options: </h2>
+      <div className="poll-form-options">
+        {pollOptions.map((option, i) => (
+          <PollOption
+            className="poll-form-option"
+            key={i}
+            name={option}
+            onDelete={(optionName) => deleteOption(optionName)}
+          />
+        ))}
+        <PollOptionCreator
+          onAddOption={(optionName) => addOption(optionName)}
+        />
+      </div>
+      <form>
+        <button
+          type="submit"
+          className="create-poll-btn"
+          onClick={(e) => handleSubmit(e)}
+        >
+          Create Poll
+        </button>
+      </form>
+    </div>
+  );
 }
 
 function PollOption(props) {
-    const {name} = props;
-    return (
-        <div className = "poll-form-option" onClick = {(() => props.onDelete(name))}>
-            <h2>{name}</h2>
-        </div>
-    )
+  const { name } = props;
+  return (
+    <div className="poll-form-option" onClick={() => props.onDelete(name)}>
+      <h2>{name}</h2>
+    </div>
+  );
 }
 
 function PollOptionCreator(props) {
-    const [name, setName] = useState("");
+  const [name, setName] = useState("");
 
-    const handleChange = (e) => setName(e.target.value);
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        props.onAddOption(name);
-        setName("");
-    }
+  const handleChange = (e) => setName(e.target.value);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.onAddOption(name);
+    setName("");
+  };
 
-    return (
-        <div className="poll-form-option-creator">
-            <form onSubmit={handleSubmit}>
-                <input type="text" className="form-input" value = {name} onChange={handleChange}/>
-            </form>
-        </div>
-    )
+  return (
+    <div className="poll-form-option-creator">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="form-input"
+          value={name}
+          onChange={handleChange}
+        />
+      </form>
+    </div>
+  );
 }
